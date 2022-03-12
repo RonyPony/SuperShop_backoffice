8fa53014ea0fc657bc0b8f1ac64c630772903c95
Ronel Cruz <rocruz@apap.com.do>
2022-03-12 14:14:18 -0400

    Actualizar .gitignore, README.md y 132 más archivos...

--- src/app/models/attachedImage.ts
+++
@@ -1,7 +0,0 @@
-export interface AttachedImage {
-    id: number,
-    name: string,
-    image: string,
-    reportId: number,
-    createdAt: Date
-}
--- src/app/models/registerRequest.ts
+++
@@ -1,9 +0,0 @@
-export interface RegisterRequest {
-    name: string
-    role: number
-    registerDate: Date
-    bornDate: Date
-    userName: string
-    email: string
-    password: string;
-}
